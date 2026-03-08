9a4f2cf3df372243e2bdf59ef24bf7ef7bfab3b6
oradziwill <aaradziwill@gmail.com>
2026-03-08 11:36:11 +0100

    fix names

--- backend/apps/billing/urls.py
+++ backend/apps/billing/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import InvoiceViewSet, ServiceViewSet
 
 router = DefaultRouter()
-router.register(r"billing/services", ServiceViewSet, basename="billing-services")
-router.register(r"billing/invoices", InvoiceViewSet, basename="billing-invoices")
+router.register(r"services", ServiceViewSet, basename="billing-services")
+router.register(r"invoices", InvoiceViewSet, basename="billing-invoices")
 
 urlpatterns = [
     path("", include(router.urls)),
